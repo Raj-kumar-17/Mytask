36ae186008d0b84b6bbae8a8f1a069a5f1fe7478
Raj Kumar M <66583607+Raj-kumar-17@users.noreply.github.com>
2023-02-17 14:47:19 +0530

    Add files via upload

---
+++ C#/OOPS App/OOPS App/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Assembly1;
+
+namespace Assembly2
+{
+    public class Customer : User
+    {
+        public override void UserFunction()
+        {
+            Console.WriteLine("Customer Panel");
+        }
+
+        internal void Displayproducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer();
+     
+
+
+        }
+    }
+}
+
---
+++ C#/OOPS App/OOPS App/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Assembly1
+{
+
+
+    public interface Product
+    {
+        public static List<string> stock = new List<string>();
+        public static List<string> products = new List<string>();
+
+        public void Displayproducts()
+        {
+            Console.WriteLine("Products:");
+            foreach (var data in products)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+
+
+    interface AddProduct : Product
+    {
+        internal void Addproduct(string product)
+        {
+            products.Add(product);
+        }
+       internal void Addproduct(string productType, string product)
+        {
+            stock.Add(product);
+        }
+
+    }
+
+    public class Admin : User,AddProduct
+    {
+        public override void UserFunction()
+        {
+            Console.WriteLine("Admin Panel");
+        }
+
+    }
+    public abstract class User:Product
+    {
+        private string name = "";
+        private string password = "";
+        private string userType = "";
+
+        public void SetUser(string name, string password, string userType)
+        {
+            this.name = name;
+            this.password = password;
+            this.userType = userType;
+        }
+
+        public void GetUser()
+        {
+            Console.WriteLine("You are Logged in as " + userType + "(" + name + ")");
+        }
+
+        public abstract void UserFunction();
+
+    }
+
+
+
+
+}
+
+
+
+
